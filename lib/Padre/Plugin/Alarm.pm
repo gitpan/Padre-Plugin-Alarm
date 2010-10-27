5baf76f7edfd1bb63b1de3896f341bf7e8fc0b08
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-27 07:50:30 +0000

    Import of AZAWAWI/Padre-Plugin-Alarm-0.10 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-Alarm
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         AZAWAWI/Padre-Plugin-Alarm-0.10.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

--- lib/Padre/Plugin/Alarm.pm
+++ lib/Padre/Plugin/Alarm.pm
@@ -1,6 +1,6 @@
 package Padre::Plugin::Alarm;
 BEGIN {
-  $Padre::Plugin::Alarm::VERSION = '0.09';
+  $Padre::Plugin::Alarm::VERSION = '0.10';
 }
 
 # ABSTRACT: Alarm Clock in Padre
@@ -208,7 +208,7 @@ Padre::Plugin::Alarm - Alarm Clock in Padre
 
 =head1 VERSION
 
-version 0.09
+version 0.10
 
 =head1 SYNOPSIS
 
